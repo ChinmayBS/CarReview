378d6e4fb46a477536bd72d5f0ac008dd4a2a777
avshet <avshet2001@gmail.com>
2021-07-28 11:03:57 +0530

    Validate fields. done - validation of username, password, and location.

--- src/controllers/SignupController.java
+++ src/controllers/SignupController.java
@@ -14,6 +14,7 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import java.util.regex.*;
 
 import java.io.IOException;
 import java.net.URL;
@@ -53,11 +54,11 @@ public class SignupController implements Initializable {
 
     private boolean validateFields(){
         if(usernameText.getText().isEmpty() || passwordText.getText().isEmpty() || locationText.getText().isEmpty()
-       || (!male.isSelected() && !female.isSelected()) ){
-            Alert alert=new Alert(Alert.AlertType.WARNING);
-            alert.setTitle("valid fields");
+       || (!male.isSelected() && !female.isSelected())|| (!(locationText.getText().matches("[A-Za-z]+")))){
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Warning");
             alert.setHeaderText("please enter all the details: ");
-            alert.setContentText("make sure you name,password,gender, and location");
+            alert.setContentText("Make sure you entered name, password, gender, and location correctly");
             alert.showAndWait();
             return false;
         }
@@ -65,12 +66,54 @@ public class SignupController implements Initializable {
     }
 
     private boolean validateName(){               //name should contain only letters numbers and underscores
-        //TODO :complete this method to name as per above comment
-        //TODO: show alert (for more details refer validate fields methods
+        if(!(usernameText.getText().matches("[A-Za-z0-9_]+")))
+        {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Warning");
+            alert.setHeaderText("Username should only contain: ");
+            alert.setContentText("1. Letters\n2. Numbers/Underscore");
+            alert.showAndWait();
+            return false;
+        }
         return true;
     }
+
     private boolean validatePassword(){            //suggest strong password using regular expression
-        //TODO: complete this method to accept strong password(use concept of regular expression)
+
+        int iPasswordScore = 0;
+        String password = passwordText.getText();
+
+        if( password.length() >= 8 )
+            iPasswordScore += 2;
+        else
+            iPasswordScore += 1;
+
+        //if it contains one digit, add 2 to total score
+        if( password.matches("(?=.*[0-9]).*") )
+            iPasswordScore += 2;
+
+        //if it contains one lower case letter, add 2 to total score
+        if( password.matches("(?=.*[a-z]).*") )
+            iPasswordScore += 2;
+
+        //if it contains one upper case letter, add 2 to total score
+        if( password.matches("(?=.*[A-Z]).*") )
+            iPasswordScore += 2;
+
+        //if it contains one special character, add 2 to total score
+        if( password.matches("(?=.*[~!@#$%^&*()_-]).*") )
+            iPasswordScore += 2;
+
+        if(iPasswordScore!=10)
+        {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Warning");
+            alert.setHeaderText("Entered password is weak ");
+            alert.setContentText("Requirements for a password:\n1. Least 8 characters long \n2. At least one Upper case " +
+                    "and lower case\n3. At least One special character.");
+            alert.showAndWait();
+            return false;
+        }
         return true;
     }
 
